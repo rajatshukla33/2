34d53ddbe1d6346e4afb1c5dc3579c748e44de70
rajatshukla33 <73328441+rajatshukla33@users.noreply.github.com>
2020-10-23 21:43:05 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
-<?php 
+/*<?php 
 
 $host = "us-cdbr-east-02.cleardb.com";
 $user = "b72ef57090ca66";
@@ -6,7 +6,7 @@ $pass = "708d1cd0";
 $db   = "heroku_fcd20689d46b3ac";
 //$conn = null;
 
-
+/*
 try {
   $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
 } catch (Exception $e) {
@@ -15,3 +15,19 @@ try {
 
 
  ?>
+*/
+<?php
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "b72ef57090ca66";
+$password = "708d1cd0";
+$db   = "heroku_fcd20689d46b3ac";
+
+// Create connection
+$conn = new mysqli($servername,$db, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
