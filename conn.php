e2c5470442918b05911e292133fd2a7d6af3726f
rajatshukla33 <73328441+rajatshukla33@users.noreply.github.com>
2020-10-24 00:28:47 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -43,6 +43,6 @@ $conn = new mysqli($servername,$db, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }*/
-echo "Connected successfully";
-?>
-*/
+//echo "Connected successfully";
+//?>
+//*/
